243b125253a6290dcebe9dcbbec1c510899708f3
A329-web <arjeeta9@gmail.com>
2024-09-10 18:18:44 +0530

    final commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import turtle
+
+#create canvas
+turtle.Screen().bgcolor("Orange")
+sc = turtle.Screen()
+sc.setup(400,300)
+turtle.title("Welcome to the turtle Window")
+
+
+#object creation
+board = turtle.Turtle()
+
+#creating a square
+for i in range(4):
+    board.forward(100)
+    board.left(90)
+    i=i+1
+    
